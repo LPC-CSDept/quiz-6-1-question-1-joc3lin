8e9a2c2bd6634b2afe8d5f78ab9dedde17b81f93
Ugalde <jocelin.uga513@gmail.com>
2023-03-21 14:33:10 -0700

    1

--- q1.hpp
+++ q1.hpp
@@ -3,9 +3,36 @@
 using namespace std;
 
 //********************
-// Declare Function Prototypes
+void getinput(int &);
+void getinput(int &, int &);
+void getinput(float &, float &);
+
 //********************
 
 // ******************************
-// Implement all your functions here
+
+void getinput(int &n1, int &n2)
+{
+    cin >> n1 >> n2;
+}
+
+void swapTwoValues(int &n1, int &n2)
+{
+    int tmp;
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+}
+void getinput(float &fnum1, float &fnum2)
+{
+    cin >> fnum1 >> fnum2;
+}
+void swapTwoValues(float &fnum1, float &fnum2)
+{
+    int tmp;
+    tmp = fnum1;
+    fnum1 = fnum2;
+    fnum2 = tmp;
+}
+
 // ******************************
